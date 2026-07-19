e9b04fbd77a88cc950b072b321ef85c61b4299a6
andradejustineakiro-a11y <andradejustineakiro@gmail.com>
2026-07-19 10:32:27 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
